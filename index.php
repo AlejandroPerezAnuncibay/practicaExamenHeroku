187ceb58b4e4ce473957b3e12b4bde894fc04649
Alejandro Pérez <56156526+AlejandroPerezAnuncibay@users.noreply.github.com>
2021-02-03 19:44:57 +0100

    a

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ function connect(){
     
 
     try{
-        $db = new PDO("mysql:eu-cdbr-west-03.cleardb.net;dbname=heroku_e91d6da0e5ae3f7",'b55227fd0484d8','7aa98dac');
+        $db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_e91d6da0e5ae3f7",'b55227fd0484d8','7aa98dac');
         echo "conexión correcta a la base de datos";
         return $db;
     }
